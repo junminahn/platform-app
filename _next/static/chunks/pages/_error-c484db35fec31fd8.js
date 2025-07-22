fba43acb687d95e99f0a0e98d638f3eed39cb66c
junminahn <36021827+junminahn@users.noreply.github.com>
2025-07-22 05:07:11 +0000

    Deploying to gh-pages from @ junminahn/trading-platform@a6038b1d7b4b77199291b27158b71e8d5359ea8f 🚀

--- _next/static/chunks/pages/_error-c484db35fec31fd8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{87156:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(10136)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(87156)),_N_E=_.O()}]);
